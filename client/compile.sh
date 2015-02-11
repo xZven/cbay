42f6e20125dd66b177517cbe18a87d13d088bc0e
xZven <manavai.teikituhaahaa@gmail.com>
2015-02-12 00:19:30 +0100

    compile client bash
    Mode acheteur a finir

--- client/compile.sh
+++ client/compile.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 #Manavai TEIKITUHAAHAA - Lorrain BALBIANI
-echo "##############################"
-echo "Compilation en version 32 bits"
-echo "##############################"
 
-gcc -m32 "mainServer.c"  -o "server32.exe" -Wall -pipe
+gcc "mainClient.c"  -o "client.exe" -Wall -pipe
 
-echo "##############################"
-echo "Compilation en version 64 bits"
-echo "##############################"
-
-gcc -m64 "mainServer.c"  -o "server64.exe" -Wall -pipe
-
-./server64.exe  -debug
+./client.exe
